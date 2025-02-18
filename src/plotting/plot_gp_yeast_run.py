b4b39032ad8f61f25e47c08df8be7cdd2d836d32
David Mets <dgmets@gamil.com>
2025-02-18 12:09:13 -0800

    moved functions to helper functions and added call to helper_functions

--- src/plotting/plot_gp_yeast_run.py
+++ src/plotting/plot_gp_yeast_run.py
@@ -6,38 +6,17 @@ import numpy as np
 import scipy as sc
 import seaborn as sns
 from sklearn.manifold import TSNE
+from helper_functions import *
+
 
 """This script runs an initial analysis on the output G-P Atlas of the yeast cross
- data from DOI_FOR_YEAST_PAPER and presented in DOI_FOR_PUB. It creates a series of figures
+ data from 10.1038/nature11867 and presented in DOI_FOR_PUB. It creates a series of figures
  presented in that pub. It is intended to be used as follows:
  python3 plot_gp_yeast_run.py [PATH TO G-P ATLAS OUTPUT]"""
 
 target_folder = sys.argv[
     1
 ]  # folder containing output of G-P Atlas when run on the Yeast cross data
-
-
-# helper functions
-def mean_absolute_percentage_error(y_true, y_pred):
-    # calculates the mean absolute percentage error of a set of predictions
-    eps = 1e-15 #minimum value to avoid underflow and allow handling of divzero
-    y_true, y_pred = np.array(y_true + EPS), np.array(y_pred)
-    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
-
-
-def MSE(y_true, y_pred):
-    # calculates the mean squared error of a set of predictions
-    y_true, y_pred = np.array(y_true), np.array(y_pred)
-    return np.mean((y_true - y_pred) ** 2)
-
-
-def calc_coef_of_det(y_true, y_pred):
-    # calculates the coefficient of determination of a model
-    y_true = np.array(y_true)
-    np.array(y_pred)
-    ssres = np.sum((y_true - y_pred) ** 2)
-    sstot = np.sum((y_true - np.mean(y_true)) ** 2)
-    return 1 - (ssres / sstot)
 
 
 # load real and predicted phenotypes. Predictions are based on phenotypes
