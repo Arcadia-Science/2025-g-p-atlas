c8943f860c1b410b1eaa9ed2929b91908daf086a
David Mets <dgmets@gamil.com>
2025-02-20 14:32:45 -0800

    formatted compare coef script

--- src/plotting/plot_compare_coef_det_two_runs.py
+++ src/plotting/plot_compare_coef_det_two_runs.py
@@ -3,20 +3,14 @@ import sys
 
 import helper_functions as hf
 import matplotlib.pyplot as plt
-import numpy as np
 import scipy as sc
-import seaborn as sns
 
 """This script compares the output of two different G-P Atlas runs and plots the
  coefficient of determination for both models"""
 
-target_folder_1 = sys.argv[
-    1
-]  # folder containing the first output of G-P Atlas 
+target_folder_1 = sys.argv[1]  # folder containing the first output of G-P Atlas
 
-target_folder_2 = sys.argv[
-    2
-]  # folder containing the second output of G-P Atlas 
+target_folder_2 = sys.argv[2]  # folder containing the second output of G-P Atlas
 
 # load real and predicted phenotypes. predictions are based on genotypes
 with open(target_folder_1 + "phens_phen_encodings_dng_attr.pk", "rb") as data:
@@ -25,7 +19,7 @@ with open(target_folder_1 + "phens_phen_encodings_dng_attr.pk", "rb") as data:
 with open(target_folder_2 + "phens_phen_encodings_dng_attr.pk", "rb") as data:
     gen_encodings_2 = pk.load(data)
 
-#calculate the coefficient of determination for both models
+# calculate the coefficient of determination for both models
 coef_det_1 = [
     hf.calc_coef_of_det(gen_encodings_1[0][n], gen_encodings_1[1][n])
     for n in range(len(gen_encodings_1[0]))
@@ -36,7 +30,7 @@ coef_det_2 = [
     for n in range(len(gen_encodings_2[0]))
 ]
 
-#plot the comparison of the two coefficients of determination
+# plot the comparison of the two coefficients of determination
 plt.plot(coef_det_2, coef_det_1, "o")
 print("coefficient of determination 1 vs. coefficient of determination 2")
 print(sc.stats.wilcoxon(coef_det_1, coef_det_2))
